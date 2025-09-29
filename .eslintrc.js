7de93559a650bcd4283585e00a67cfde63c511f8
ffernnando <ffernnando192@gmail.com>
2025-09-29 10:42:55 +0200

    Lint fix #2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
-export default {
+module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'jest/globals': true
+    'jest/globals': true,
+    'node': true
   },
   'extends': [
     'eslint:recommended',
