a8985c45d3b57fc4abc0a01c73e8d3cda1e304df
ffernnando <ffernnando192@gmail.com>
2025-09-30 10:20:43 +0200

    .....

--- e2e-tests/e2e.spec.js
+++ e2e-tests/e2e.spec.js
@@ -9,7 +9,7 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-  test('front page can be opened', async ({ page }) => {
+  test('clicing on a character opens its details page', async ({ page }) => {
     await page.goto('/pokemon/ivysaur')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Home')).toBeVisible()
